ec09083722e6fae3ce619ea5592c7799563943d8
Connor Kouznetsov <161536397+C-Kuzy@users.noreply.github.com>
2025-07-11 16:19:18 -0400

    Update Assignment_4_Code.asm

--- Hw-Assignments - 25%/Assignment-4/Assignment_4_Code.asm
+++ Hw-Assignments - 25%/Assignment-4/Assignment_4_Code.asm
@@ -22,11 +22,32 @@
                                 then the result is loaded into the destination register. For our result, every 0 will
                                 become 1 and every 1 will become zero."
 
-    // QUESTION #2:
+    // QUESTION #2: Values to contain in register a0: { a. -2037 | b. 4169 | c. 0xD6A | d. 1010110110110000010011_2 }
 
+        "Part A: -2037"
 
+            addi a0, x0, -2037      # Arithmetic Instruction: 
 
-    // QUESTION #3:
+        "Part B: 4169"
+
+            lui a0, 0x1             # Data Transfer U Format: Loads 20-it upper 
+            addi a0, a0, 169        # Arithmetic Instruction: 
+
+        "Part C: 3434"
+
+            addi x0, x0, 0x6DA      # Arithmetic Instruction: 
+
+        "Part D: 1010110110110000010011_2"
+
+            lui a0, 0xADB           # Data Transfer U Format: Loads 20-it upper 
+            addi a0, a0, 0x13       # Arithmetic Instruction: 
+
+    // QUESTION #3:        Integer Variables: a, b, c, d, e, f      ||      Registers: t0, t1, t2, t3, t4, t5
+                                                                          #Equivalent: x5, x6, x7, x28, x29, x30
+
+        add t0, t1, t2 # t0 = t1 + t2
+        
+        
 
     // QUESTION #4:
 
