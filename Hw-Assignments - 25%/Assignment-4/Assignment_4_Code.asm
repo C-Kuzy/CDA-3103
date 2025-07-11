e49213aacb67064ec9473207aca16d73e0356837
Connor Kouznetsov <161536397+C-Kuzy@users.noreply.github.com>
2025-07-11 15:33:05 -0400

    Update Assignment_4_Code.asm

--- Hw-Assignments - 25%/Assignment-4/Assignment_4_Code.asm
+++ Hw-Assignments - 25%/Assignment-4/Assignment_4_Code.asm
@@ -2,7 +2,7 @@
  Author: C-Kuzy
  Description: CDA-3103 Computer Organization 
                     Major Assignment #4
-
+                      RISC-V Assembly
 */
 
     // Question #1: risc-v-asm-manual.pdf
@@ -10,15 +10,17 @@
         "Reference 3.1.1.6 NOT: Given the following syntax statement:'not rd, rs1'"
             "NOT, known as a bit-wise invert operation performs one's complment arithmetic"                     
 
-                "Translation: xori rd, rs1, -1  [ -1 = 0xFFFFFFFF]"
+                "Translation: xori rd, rs1, -1  [-1 = 0xFFFFFFFF]"
 
-                    /* Where 
+                    /* Description of the the code instructions 
                         rs1     source register 1
                         rd      destination register 
                     */
 
-            "Why does it work?: The translation works correctly because "
-
+            "Why does it work?: The translation works correctly because NOT instructions flip each bit of the register.
+                                NOT translates to the exclusive OR operation XORI which implements the negation. Which 
+                                then the result is loaded into the destination register.
+            "
 
     // QUESTION #2:
 
