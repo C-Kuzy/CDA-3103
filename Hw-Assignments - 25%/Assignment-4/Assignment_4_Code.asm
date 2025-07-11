d985cd1a9dce491678b73b15e75d801ebb708aab
Connor Kouznetsov <161536397+C-Kuzy@users.noreply.github.com>
2025-07-11 14:43:37 -0400

    Update Assignment_4_Code.asm

--- Hw-Assignments - 25%/Assignment-4/Assignment_4_Code.asm
+++ Hw-Assignments - 25%/Assignment-4/Assignment_4_Code.asm
@@ -1,11 +1,13 @@
 /*
  Author: C-Kuzy
- Description: CDA-3103 Computer Organization Major Assignment #4
+ Description: CDA-3103 Computer Organization 
+                    Major Assignment #4
+
 */
 
     // Question #1 
 
-        
+        "Considering that 'risc-v-isa-spec.pdf' doesn't include 'NOT', we can translate 'not rd, rs"
 
     // QUESTION #2:
 
