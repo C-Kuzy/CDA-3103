efd0877be1104c8f1bf8e952a2bf9f1ccba267ef
Connor Kouznetsov <161536397+C-Kuzy@users.noreply.github.com>
2025-07-12 13:33:42 -0400

    Update Assignment_4_Code.asm

--- Hw-Assignments - 25%/Assignment-4/Assignment_4_Code.asm
+++ Hw-Assignments - 25%/Assignment-4/Assignment_4_Code.asm
@@ -29,7 +29,7 @@
         "Part A: -2037"
 
             addi a0, zero, -2037    # Arithmetic Instruction: Load's x0 and add value -2037 into register a0 
-                                    # Meaning: a0 = x0 + (-2037)
+                                    # Meaning: a0 = x0 + (-2037) = -2037
                                         
                                         /* Description of the code instructions
                                             -2037      subtract by value of 2037
@@ -39,26 +39,26 @@
 
         "Part B: 4169"
 
-            lui a0, 0x1             # Data Transfer U Format: Loads 20 upper bits (a0 == 0x1) && 12 bits to the left
+            lui a0, 0x1             # Load Upper Immediate: Loads 20 upper bits (a0 == 0x1) && 12 bits to the left
             addi a0, a0, 73         # Load/Store Instruction: Load's value 4096 to register a0 (1*2^12)
-                                    # Arithmetic Instruction: Load's addition to register a0 and 73
+                                    # Arithmetic Instruction: Addition operation to load stored in a0 plus 73 to a0
                                     # Meaning: a0 = 4096 + 73 = 4169
 
                                         /* Description of the code instructions
-                                            0x1        calculates value after shifting left 12 bits
+                                            0x1        calculates value after shifting left 12 bits = 4096
                                             73         value left over to be added
                                             a0         register storage
                                         */
 
         "Part C: 0xD6A = 3434"       
 
-            lui a0, 0x1             # Data Transfer U Format: Loads 20 upper bits (a0 == 0x1) && 12 bits to the left
+            lui a0, 0x1             # Load Upper Immediate: Loads 20 upper bits (a0 == 0x1) && 12 bits to the left
             addi a0, a0, 0xD6A      # Load/Store Instruction: Load's value 4096 to register a0 (1*2^12)
-                                    # Arithmetic Instruction: Load's the value 0x6DA or -662 into register a0
+                                    # Arithmetic Instruction: Addition operation to load stored in a0 plus -662 to a0
                                     # Meaning: a0 = 4096 + (-662) = 3434
 
                                         /* Description of the code instructions
-                                            0x1        calculates value after shifting left 12 bits
+                                            0x1        calculates value after shifting left 12 bits = 4096
                                             -662 or 0x6DA     subtraction by value of 662
                                             a0         register storage 
                                         */
@@ -66,25 +66,58 @@
         "Part D: 1010110110110000010011_2 = 2845715"   // CONVERT BINARY TO HEXADECIMAL: 2B6C13 >> Make 8-Bit: 002B6C13
                                                        // >> Count 5 From Left && + 1: 002B7C13 
 
-            lui a0, 0x2B7           # Data Transfer U Format: Loads 20 upper bits (a0 == 0x2B7) && 12 bits to the left
-            addi a0, a0, 0xC13      # Load/Store Instruction: first a0 storage value is 710912
-                                    # Arithmetic Instruction: 
-                                    # Meaning: a0 = 
+            lui a0, 0x2B7           # Load Upper Immediate: Loads 20 upper bits (a0 == 0x2B7) && 12 bits to the left 2B7
+            addi a0, a0, 0xC13      # Load/Store Instruction: Load's value 2846720 to register a0
+                                    # Arithmetic Instruction: Addition operation to load stored in a0 plus -1005 to a0
+                                    # Meaning: a0 = 2846720 + (-1005) = 2845715
 
                                         /* Description of the code instructions
-                                            0x2B7      addition by value 
-                                            0xC13       
+                                            0x2B7      addition by value = 2846720
+                                            0xC13      subtraction by value = -1005
                                             a0         register storage
                                         */
 
     // QUESTION #3:        Integer Variables: a, b, c, d, e, f      ||      Registers: t0, t1, t2, t3, t4, t5
-                                                                          #Equivalent: x5, x6, x7, x28, x29, x30
 
-        addi t0, t1, imm # t0 = t1 + imm
+                                                                                /* PSUEDOCODE INSTRUCTIONS */
+                    """     if (( a > b ) && ( c > d )) {               IF A is greater than B and C is greater than D    
+                                e = e / 4;                              then, compute e = (e / 4)
+                                f = f * 4;                              then, compute f = (f * 4)
+                            }
+                            else {                                      IF A is not greater than B or C is not greater than D
+                                e = ( a - b ) + ( c - d );              then, compute e = ( a - b ) + ( c - d )
+                                f = ( a + b ) - ( c + d );              then, compute f = ( a + b ) - ( c + d )
+                            }
+                    """
+
+        chk_a_grtr_b:
+            blt t1, t0, chk_c_grtr_d       # Checks the following "if (t1 < t0) go to chk_c_grtr_d
+            j else_op                      #
+
+        chk_c_grtr_d:
+            bge t2, t3, variables          # if t
+            addi t0, t1,                # t0 = t1 + imm 
+
+        
+            blt t0, t1, target             # if t0 < t1 then target
+
+        then_op:
+
+        j end chck
+
+        else_op:
+
+
         
         
 
     // QUESTION #4:
+
+
+
+
+
+
 
         rev_char:
             addi sp, sp, -16         # Allocate stack space for 4 registers
